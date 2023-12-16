c82ce35c310422072aacae9a691a69ea9691c559
iru-Y <yuri_matteus@hotmail.com>
2023-12-16 13:45:50 -0300

    implementando a logo do header

--- raiox_project_flutter/lib/main.dart
+++ raiox_project_flutter/lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
   const MyApp({Key? key});
 
   @override
--- raiox_project_flutter/lib/ui/view/app_overview/header_view.dart
+++ raiox_project_flutter/lib/ui/view/app_overview/header_view.dart
@@ -7,7 +7,7 @@ class HeaderView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 45),
       height: 144,
       width: double.infinity,
       color: CustomColors.DEFAULT_BLUE,
@@ -15,24 +15,48 @@ class HeaderView extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           GestureDetector(
-              onTap: () => Navigator.pop(context),
-              child:
-                  Image.asset('assets/icons/back-arrow-icon.png', scale: 2.5)),
-          const Text(
-            'Raio-x',
-            style: TextStyle(
-              color: CustomColors.DEFAULT_YELLOW,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w900,
-                fontSize: 30),
+            onTap: () => Navigator.pop(context),
+            child: Image.asset('assets/icons/back-arrow-icon.png', scale: 2.5, color: Colors.white,),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/images/logo_raio_x_image.png',
+                scale: 10,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'RAIO X ',
+                    style: TextStyle(
+                      color: CustomColors.DEFAULT_YELLOW,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w900,
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'RADIOLOGIA ODONTOLÓGICA',
+                    style: TextStyle(
+                      color: CustomColors.DEFAULT_YELLOW,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w900,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
           GestureDetector(
             onTap: () => Navigator.pop(context),
-            child: Image.asset(
-              'assets/icons/menu-3-icon.png',
-              scale: 2.5,
-            ),
-          )
+            child: Image.asset('assets/icons/menu-3-icon.png', scale: 2.5, color: Colors.white),
+          ),
         ],
       ),
     );
